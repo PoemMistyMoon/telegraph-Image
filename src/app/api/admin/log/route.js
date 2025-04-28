9c90fc2d7209dfaa1588fae095c3476b3f24cde3
PoemMistyMoon <167159446+PoemMistyMoon@users.noreply.github.com>
2025-04-28 17:09:44 +0800

    Update route.js

--- src/app/api/admin/log/route.js
+++ src/app/api/admin/log/route.js
@@ -2,38 +2,47 @@ import { NextResponse } from "next/server";
 import { headers } from 'next/headers'
 import { getRequestContext } from '@cloudflare/next-on-pages';
 
-// 定义 CORS 头
+// ...
+
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers': 'Content-Type',
-  'Access-Control-Max-Age': '86400', // 24小时
+  'Access-Control-Max-Age': '86400', // 24 hours
   'Content-Type': 'application/json'
 };
 
 export const runtime = 'edge';
 
 export async function POST(request) {
-  // 获取客户端的请求上下文
+  // 获取客户端的IP地址
   const { env, cf, ctx } = getRequestContext();
   
   try {
-    let { page, query } = await request.json();
+    let { page, query } = await request.json()
 
-    // 如果提供了查询参数
     if (query) {
       const ps = env.IMG.prepare(`
-        SELECT tgimglog.id, tgimglog.url, tgimglog.referer, tgimglog.ip, tgimglog.time, imginfo.rating, imginfo.total
+        SELECT tgimglog.*, imginfo.rating, imginfo.total
         FROM tgimglog
         JOIN imginfo ON tgimglog.url = imginfo.url
-        WHERE tgimglog.url LIKE '%${query}%'
-        ORDER BY tgimglog.id DESC
+        WHERE tgimglog.url LIKE '%${query}%' 
+        OR tgimglog.id LIKE '%${query}%' 
+        OR tgimglog.referer LIKE '%${query}%'
+        OR tgimglog.ip LIKE '%${query}%' 
+        OR tgimglog.time LIKE '%${query}%'
+        ORDER BY tgimglog.id DESC 
         LIMIT 10 OFFSET ${page} * 10
       `);
       const { results } = await ps.all();
+      
       const total = await env.IMG.prepare(`
-        SELECT COUNT(*) as total
-        FROM tgimglog
-        WHERE url LIKE '%${query}%'
+        SELECT COUNT(*) as total 
+        FROM tgimglog 
+        WHERE tgimglog.url LIKE '%${query}%' 
+        OR tgimglog.id LIKE '%${query}%' 
+        OR tgimglog.referer LIKE '%${query}%'
+        OR tgimglog.ip LIKE '%${query}%' 
+        OR tgimglog.time LIKE '%${query}%'
       `).first();
 
       return Response.json({
@@ -45,20 +54,16 @@ export async function POST(request) {
         "total": total.total
       });
     } else {
-      // 如果没有提供查询参数，返回所有数据
       const ps = env.IMG.prepare(`
-        SELECT tgimglog.id, tgimglog.url, tgimglog.referer, tgimglog.ip, tgimglog.time, imginfo.rating, imginfo.total
+        SELECT tgimglog.*, imginfo.rating, imginfo.total
         FROM tgimglog
         JOIN imginfo ON tgimglog.url = imginfo.url
-        ORDER BY tgimglog.id DESC
-        LIMIT 10 OFFSET ${page} * 10
+        ORDER BY tgimglog.id DESC 
+        LIMIT 10 OFFSET  ${page} * 10
       `);
       const { results } = await ps.all();
-      const total = await env.IMG.prepare(`
-        SELECT COUNT(*) as total
-        FROM tgimglog
-      `).first();
-
+      const total = await env.IMG.prepare(`SELECT COUNT(*) as total FROM tgimglog`).first();
+      
       return Response.json({
         "code": 200,
         "success": true,
@@ -77,6 +82,6 @@ export async function POST(request) {
     }, {
       status: 500,
       headers: corsHeaders,
-    });
+    })
   }
 }
